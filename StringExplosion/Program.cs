57986e1c1d97db0e3ca264d30149bd55a0163a85
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2020-12-10 01:46:31 +0200

    Add files via upload

---
+++ StringExplosion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // abv > 1 > 1 > 2 > 2asdasd
+
+            string input = Console.ReadLine();
+            int bomb = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currcharacter = input[i];
+
+                if (currcharacter == '>')
+                {
+                    bomb += int.Parse(input[i + 1].ToString());
+                    continue;
+
+                }
+
+                if (bomb > 0)
+                {
+                    input = input.Remove(i, 1);
+                    bomb--;
+                    i--;
+                }
+            }
+
+          
+
+            Console.WriteLine(input);
+
+        }
+    }
+}
