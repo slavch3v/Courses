1a51a79fa7fc18e51b5f89448034d611bf1f3847
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2020-12-10 01:39:20 +0200

    Add files via upload

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            // string result = string.Empty;
+
+            foreach (char ch in command)
+            {
+                int last = (int)ch + 3;
+                char word   = (char)last;
+                Console.Write(word);
+
+            }
+        }
+    }
+}
