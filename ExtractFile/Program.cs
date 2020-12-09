443c27e1fe5430bd619c5e96e4db8f0b85b7544f
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2020-12-10 01:42:59 +0200

    Add files via upload

---
+++ ExtractFile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(@"\");
+
+             string lastFile = array[array.Length - 1];
+            string[] lastAraay = lastFile.Split(".");
+
+            string firstWord = lastAraay[0];
+
+            string secondWord = lastAraay[1];
+
+            Console.WriteLine($"File name: {firstWord}");
+            Console.WriteLine($"File extension: { secondWord} ");
+        }
+    }
+}
