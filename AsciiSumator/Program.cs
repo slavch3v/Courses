c32675a26063cf42f4a4335ee9eb2c3e10cef22b
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2020-12-10 01:38:27 +0200

    Add files via upload

---
+++ AsciiSumator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AsciiSumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+
+            string collection = Console.ReadLine();
+
+            int one = first;
+            int two = second;
+
+            int total = 0;
+            for (int i = 0; i < collection.Length; i++)
+            {
+                char curr = collection[i];
+                int sum = curr;
+
+                if (sum > one && sum < two || sum > two && sum < one)
+                {
+                    total += sum;
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
