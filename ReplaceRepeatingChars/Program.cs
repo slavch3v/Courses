bb237edbc5e2b6d66e21cfa0659e3dd68b186bc7
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2020-12-10 01:44:47 +0200

    Add files via upload

---
+++ ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] array = Console.ReadLine().ToCharArray();
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < array.Length - 1 ; i++)
+            {
+                if (array[i] != array[i + 1])
+                {
+                    sb.Append(array[i]);
+                }
+            }
+
+            sb.Append(array[array.Length - 1]);
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
