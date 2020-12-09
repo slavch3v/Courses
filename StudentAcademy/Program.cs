a7caa1b8683813367f484aadf0c2eea8faf4a876
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2020-12-10 01:47:52 +0200

    Add files via upload

---
+++ StudentAcademy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> studentWithGrade = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+
+                if (!studentWithGrade.ContainsKey(name))
+                {
+                    studentWithGrade.Add(name, new List<double>());
+
+                }
+
+                studentWithGrade[name].Add(grade);
+                    
+            }
+
+
+            foreach (var item in studentWithGrade.Where(x=>x.Value.Average(x => x)>=4.50)
+                .OrderByDescending(y =>y.Value.Average(z=>z)))
+            {
+                Console.WriteLine($"{item.Key} –> {item.Value.Average(x =>x):f2}");
+            }
+        }
+    }
+}
