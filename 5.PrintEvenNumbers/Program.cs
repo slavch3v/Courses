72c1b1fbd4fa3c33cbef9e09d67b55db89133783
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2021-01-24 14:12:29 +0200

    Add files via upload

---
+++ 5.PrintEvenNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _5.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queues = new Queue<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] % 2 == 0)
+                {
+                    queues.Enqueue(input[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ",queues));
+        }
+    }
+}
