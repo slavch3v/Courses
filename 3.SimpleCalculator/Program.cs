5ed9cde858a66f3d81bf60dd6889cc1ab159bbaf
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2021-01-24 14:06:04 +0200

    Add files via upload

---
+++ 3.SimpleCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Stack<string> stack = new Stack<string>(input.Reverse());
+
+            var a = 0;
+
+            while (stack.Count > 1)
+            {
+                int leftNum = int.Parse(stack.Pop());
+                string sign = stack.Pop();
+                int rightNum = int.Parse(stack.Pop());
+
+                if (sign == "+")
+                {
+                   a =   leftNum + rightNum;
+                    stack.Push(a.ToString());
+                }
+                else
+                {
+                    a = leftNum - rightNum;
+                    stack.Push(a.ToString());
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', stack));
+
+        }
+    }
+}
