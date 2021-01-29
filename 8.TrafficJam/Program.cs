206ca48379afb805991de823d119c1e844671ff8
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2021-01-29 08:41:34 +0200

    Add files via upload

---
+++ 8.TrafficJam/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            Queue<string> cars = new Queue<string>();
+            int count = 0;
+
+            while (command != "end")
+            {
+                if (command != "green")
+                {
+                    cars.Enqueue(command);
+                }
+                else if (command == "green")
+                {
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            Console.WriteLine(cars.Dequeue() + " passed!");
+
+                            count++;
+                        }
+                      
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
