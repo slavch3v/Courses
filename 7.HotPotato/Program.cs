472df825a76141e8223c2f96223d9b234d43fda7
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2021-01-29 08:39:46 +0200

    Add files via upload

---
+++ 7.HotPotato/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            int num = int.Parse(Console.ReadLine());
+
+            Queue<string> result = new Queue<string>(names);
+            int count = 0;
+
+            while (result.Count > 1)
+            {
+                count++;
+
+                string kid = result.Dequeue();
+
+                if (count == num )
+                {
+                    count = 0;
+                    Console.WriteLine("Removed "+ kid );
+                }
+                else
+                {
+                    result.Enqueue(kid);
+                }
+            }
+
+            Console.WriteLine("Last is " + result.Dequeue());
+        }
+    }
+}
