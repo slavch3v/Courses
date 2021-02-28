bc7a8ec69520e2daef581e395f2c394b286ea3a6
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2021-02-28 21:30:27 +0200

    Add files via upload

---
+++ ReverseNumberswithaStack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace ReverseNumberswithaStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stackBabyy = new Stack<int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                stackBabyy.Push(array[i]);
+            }
+
+            Console.WriteLine(string.Join(" ", stackBabyy));
+        }
+    }
+}
