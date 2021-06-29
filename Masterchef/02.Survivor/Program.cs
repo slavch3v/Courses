1c51245715d18025d92514d76755b5fcdc738bbf
Biser Slavchev <74625447+slavch3v@users.noreply.github.com>
2021-06-29 18:46:09 +0300

    Add files via upload

---
+++ Masterchef/02.Survivor/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var jagged = new char[lines][];
+            var myTokens = 0;
+            var OpponentedTokens = 0;
+
+
+            //"up", "down", "left", "right"
+
+            // •	"Find  ,•	"Opponent , •	"Gong" -
+
+            for (int i = 0; i < lines; i++)
+            {
+                var tokenArgs = Console.ReadLine()
+                    .Split(' ')
+                    .ToList();
+
+                jagged[i] = new char[tokenArgs.Count];
+
+                for (int j = 0; j < tokenArgs.Count; j++)
+                {
+                    jagged[i][j] = tokenArgs[j].First();
+                }
+            }
+
+            var command = Console.ReadLine();
+
+            while (command != "Gong")
+            {
+                var commandArgs = command
+                    .Split(' ')
+                    .ToList();
+
+                var row = int.Parse(commandArgs[1]);
+                var col = int.Parse(commandArgs[2]);
+
+                if (commandArgs[0] == "Find")
+                {
+                    if (jagged.Length > row && row >= 0)
+                    {
+                        if (jagged[row].Length > col && col >= 0)
+                        {
+                            if (jagged[row][col] == 'T')
+                            {
+                                jagged[row][col] = '-';
+                                myTokens++;
+                            }
+                        }
+                    }
+                }
+                else if (commandArgs[0] == "Opponent")
+                {
+                    var direction = commandArgs[3];
+                    if (jagged.Length > row && row >= 0)
+                    {
+                        if (jagged[row].Length > col && col >= 0)
+                        {
+                            if (jagged[row][col] == 'T')
+                            {
+                                jagged[row][col] = '-';
+                                OpponentedTokens++;
+                            }
+
+                            if (direction == "up")
+                            {
+                                while (--row >= 0)
+                                {
+                                    if (jagged[row].Length - 1 >= col)
+                                    {
+                                        if (jagged[row][col] == 'T')
+                                        {
+                                            jagged[row][col] = '-';
+                                            OpponentedTokens++;
+                                        }
+                                    }
+                                }
+                            }
+                            else if (direction == "down")
+                            {
+                                while (++row < jagged.Length)
+                                {
+                                    if (jagged[row].Length - 1 >= col)
+                                    {
+                                        if (jagged[row][col] == 'T')
+                                        {
+                                            jagged[row][col] = '-';
+                                            OpponentedTokens++;
+                                        }
+                                    }
+                                }
+                            }
+                            else if (direction == "left")
+                            {
+                                while (--col >= 0)
+                                {
+                                    if (jagged[row][col] == 'T')
+                                    {
+                                        jagged[row][col] = '-';
+                                        OpponentedTokens++;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                while (++col < jagged[row].Length)
+                                {
+                                    if (jagged[row][col] == 'T')
+                                    {
+                                        jagged[row][col] = '-';
+                                        OpponentedTokens++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            for (int i = 0; i < lines; i++)
+            {
+                var cols = jagged[i].Length;
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{jagged[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Collected tokens: {myTokens}");
+            Console.WriteLine($"Opponent's tokens: {OpponentedTokens}");
+        }
+    }
+}
